0883c161ad196965e44222822e5326450e93d423
noor <noorridha028@gmail.com>
2023-01-05 00:55:01 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>I made this</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
     </div>
---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links() {
+  return (
+    <div>
+      <h1>Links</h1>
+      <a href="www.github/noera"> </a>
+      <a href="www.linkedin/noera"> </a>
+    </div>
+  );
+}
+
+export default Links;
